4e68217cf7269ae369d6aeb8af29f780e1c13e94
kiwibrie <brianne0128@gmail.com>
2021-06-05 22:01:49 -0500

    working base

--- src/main/java/base/App.java
+++ src/main/java/base/App.java
@@ -6,10 +6,36 @@ package base;
  */
 import java.util.Scanner;
 
+import static java.lang.Math.pow;
+
 public class App {
     static Scanner in = new Scanner(System.in);
 
     public static void main(String[] args){
         App myApp = new App();
+
+        double principal = myApp.prompt("principal amount");
+        double rate = myApp.prompt("rate");
+        double years = myApp.prompt("number of years");
+        double compounds = myApp.prompt("number of times the interest is compounded per year");
+
+        double interest = myApp.calculateInterest(principal, rate, years, compounds);
+
+        myApp.printOutput(principal, rate, years, compounds, interest);
+    }
+
+    public void printOutput(double prin, double rate, double years, double comp, double interest){
+        System.out.printf("$%.2f invested at %.1f%% for %d years compounded %d times " +
+                "per year is $%.2f.", prin, rate, (int) years, (int) comp, interest);
+    }
+
+    public double calculateInterest(double prin, double rate, double years, double comp){
+        double convertedRate = rate/100;
+        return prin * pow(1 + (convertedRate/comp), comp*years);
+    }
+
+    public double prompt(String prompt){
+        System.out.printf("What is the %s? ", prompt);
+        return in.nextDouble();
     }
 }
